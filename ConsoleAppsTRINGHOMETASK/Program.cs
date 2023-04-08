9784f7a7559daf3990b7b833d8c2857fef469625
shahnaz0101 <sahnazhuseynzade49@gmail.com>
2023-04-08 13:15:59 +0400

    Add project files.

---
+++ ConsoleAppsTRINGHOMETASK/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleAppsSTRINGHOMETASK
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] values = { "Samaya", "Vugar", "Novruz", "Narmina", "Edward" };
+
+            foreach (string value in values) 
+            {
+                Console.Write(value + ' ');
+            }
+         Array.Sort(values);
+            Console.WriteLine("Sorted; ");
+            foreach (string value in values)
+            {
+                Console.Write(value + ' ');
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
